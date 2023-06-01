3abf1219392fc6d63d2adbc293f6d5bbd9fd875f
Natalie Jameson <natalie@natalie.wtf>
2023-06-01 10:01:43 -0700

    Import from monorepo

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # tee 
 
-A small adapter object to let you both write to stderr *and* capture that output.
+A small adapter object to let you both write to stderr/stdout *and* capture that output.
+
+See the docs on `tee::Tee` for more details on how it works.
+
+## Adding to a project
+
+```toml
+# Cargo.toml
+[dependencies]
+tee = { git = "https://github.com/nataliejameson/tee", tag = "0.1.0" }
+```
+
+## Example Usage
+
+```rust
+fn main() {
+    let tee_err = Tee::new(std::io::stderr())?;
+    let res = std::process::Command::new(["sh"])
+        .args(["-c", "echo \"fail\" >&2; exit 1;"])
+        .stderr(tee_err.clone())
+        .output()?
+        .unwrap();
+    assert_eq!(0, res.stderr.len());
+    let stderr_out = String::from_utf8(tee_err.get_output().unwrap()).unwrap();
+    assert_eq!("fail\n", stderr_out);
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
 use std::fmt::Debug;
 use std::fmt::Formatter;
 use std::fs::File;
-use std::io::Read;
+use std::io::Seek;
 use std::io::Write;
+// This only works on unix for right now because we use read_at()
+#[cfg(unix)]
+use std::os::unix::fs::FileExt;
 use std::process::Stdio;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
-
-use tempfile::NamedTempFile;
+use std::time::Duration;
 
 struct TeeData {
-    temp_file: NamedTempFile,
+    temp_file: File,
     stop_signal: Arc<AtomicBool>,
     thread: Option<std::thread::JoinHandle<anyhow::Result<()>>>,
 }
@@ -19,8 +21,7 @@ struct TeeData {
 impl Debug for TeeData {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!(
-            "TeeData(temp_file: \"{}\", stopped: {}",
-            self.temp_file.path().to_string_lossy(),
+            "TeeData(stopped: {})",
             self.stop_signal.load(Ordering::Relaxed)
         ))
     }
@@ -28,16 +29,21 @@ impl Debug for TeeData {
 
 impl TeeData {
     fn new<T: Write + Sync + Send + 'static>(mut stdio: T) -> anyhow::Result<Self> {
-        let temp_file = NamedTempFile::new()?;
-        let path = temp_file.path().to_path_buf();
+        let temp_file = tempfile::tempfile()?;
+        let file = temp_file.try_clone()?;
         let stop_signal = Arc::new(AtomicBool::new(false));
         let thread_stop_signal = stop_signal.clone();
         let thread = std::thread::spawn(move || -> anyhow::Result<()> {
-            let mut file = std::fs::File::open(path)?;
             let mut buf = [0x00; 1024];
+            let mut offset: u64 = 0;
             while !thread_stop_signal.load(Ordering::Relaxed) {
-                let read_bytes = file.read(&mut buf)?;
+                let read_bytes = file.read_at(&mut buf, offset)?;
+                offset += read_bytes as u64;
                 stdio.write_all(&buf[0..read_bytes])?;
+                if read_bytes == 0 {
+                    // No need to busy loop on this if nothing's going on, a small pause is fine.
+                    std::thread::sleep(Duration::from_millis(5));
+                }
             }
 
             Ok(())
@@ -59,6 +65,23 @@ impl Drop for TeeData {
     }
 }
 
+/// Both captures output from a stdio object and relays it to that original object
+///
+/// Tee is mostly used for things like stderr, where one might want to show the user the output
+/// of a command on their terminal, but the program also wants to parse some data from that stderr
+/// stream.
+///
+/// Tee does this by opening an unnamed temporary file and passing that handle to `Stdio`, which
+/// can be used by [`std::process::Process`]. Everything that would be written to the stdio object
+/// is thus written to this temp file.
+///
+/// It also spawns a thread in the background that watches this temporary file, and if any new data
+/// was written to it, it relays that to the original stdio object.
+///
+/// When all instances of the [`Tee`] object are dropped, the background thread is stopped, and no
+/// more data is written to the original stdio object. If all instances of [`Tee`] *and* all
+/// [`Stdio`] that were created with [`Tee::stdio`] are dropped, then the OS will clean up the
+/// unnamed temporary file, as all handles to it should have been closed.
 #[derive(Clone, Debug)]
 pub struct Tee(Arc<TeeData>);
 
@@ -67,14 +90,43 @@ impl Tee {
         Ok(Self(Arc::new(TeeData::new(stdio)?)))
     }
 
+    /// Get the output from the open temporary file
     pub fn get_output(&self) -> anyhow::Result<Vec<u8>> {
-        Ok(std::fs::read(self.0.temp_file.path())?)
+        let mut file = self.0.temp_file.try_clone()?;
+        let len = file.stream_position()?;
+        let mut buf = vec![0x00; len as usize];
+        let mut offset = 0;
+        while offset < len {
+            let read_bytes = self.0.temp_file.read_at(buf.as_mut_slice(), offset)? as u64;
+            offset += read_bytes;
+        }
+        Ok(buf)
+    }
+
+    /// Get a [`std::process::Stdio`] instance from this Tee instance.
+    ///
+    /// Note that this duplicates the underlying temp file handle, so as long as the Tee object
+    /// and the Stdio object stick around, that unnamed temporary file will exist.
+    pub fn stdio(&self) -> Stdio {
+        let file: File = self.0.temp_file.try_clone().unwrap();
+        Stdio::from(file)
     }
 }
 
-impl From<Tee> for Stdio {
-    fn from(t: Tee) -> Self {
-        let file: File = (*t.0.temp_file.as_file()).try_clone().unwrap();
-        Stdio::from(file)
+#[cfg(test)]
+mod test {
+    use crate::Tee;
+
+    #[test]
+    fn tee_tees() {
+        let tee_err = Tee::new(std::io::stderr()).unwrap();
+        let res = std::process::Command::new("sh")
+            .args(["-c", "echo \"test failure string\" >&2; exit 1;"])
+            .stderr(tee_err.stdio())
+            .output()
+            .unwrap();
+        assert_eq!(0, res.stderr.len());
+        let stderr_out = String::from_utf8(tee_err.get_output().unwrap()).unwrap();
+        assert_eq!("test failure string\n", stderr_out);
     }
 }
